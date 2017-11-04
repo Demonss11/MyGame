b544fbbed9753acf70ead19828704c995a6cc23d
Dmitry Belousov <Demonss11@gmail.com>
2017-11-04 13:50:06 +0400

    попытка 1

---
+++ MyGame/alien_invasion.py
@@ -0,0 +1,32 @@
+import sys
+import pygame
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+from pygame.sprite import Group
+from alien import Alien
+
+
+def run_game():
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    
+    pygame.display.set_caption("Alien Invasion")
+
+    ship = Ship(ai_settings,screen)
+
+    alien = Alien(ai_settings, screen)
+    aliens = Group()
+    gf.create_fleet(ai_settings, screen, ship, aliens)
+
+    bullets = Group()
+    
+    while True:
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        gf.update_bullets(bullets, ship)
+        gf.update_aliens(aliens)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+
+run_game()
