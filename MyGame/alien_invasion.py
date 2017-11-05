d26d03def6917aa9aeeb9e9897493a2079bf8861
Dmitry Belousov <Demonss11@gmail.com>
2017-11-06 01:59:48 +0400

    Завершение игры

--- MyGame/alien_invasion.py
+++ MyGame/alien_invasion.py
@@ -6,7 +6,7 @@ import game_functions as gf
 from pygame.sprite import Group
 from alien import Alien
 from game_stats import GameStats
-
+from button import Button
 
 def run_game():
     pygame.init()
@@ -14,6 +14,8 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     
     pygame.display.set_caption("Alien Invasion")
+
+    play_button = Button(ai_settings, screen, "Play")
 
     stats = GameStats(ai_settings)
 
@@ -26,12 +28,12 @@ def run_game():
     bullets = Group()
     
     while True:
-        gf.check_events(ai_settings, screen, ship, bullets)
+        gf.check_events(ai_settings, screen, stats, play_button,ship, bullets)
         if stats.game_active:
 
             ship.update()
             gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
             gf.update_aliens(ai_settings,stats,screen,ship, aliens, bullets)
-            gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+        gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button)
 
 run_game() # запуск игры
