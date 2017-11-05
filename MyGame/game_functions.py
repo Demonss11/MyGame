ee9242c3f399165ce64bbdd3d3823b1bcd79aa6d
Dmitry Belousov <Demonss11@gmail.com>
2017-11-06 01:19:02 +0400

    Завершение игры

--- MyGame/game_functions.py
+++ MyGame/game_functions.py
@@ -114,15 +114,26 @@ def change_fleet_direction(ai_setting, aliens):
 
 
 def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
-    stats.ships_left -= 1
+    if stats.ships_left > 0:
+        stats.ships_left -= 1
 
-    aliens.empty()
-    bullets.empty()
+        aliens.empty()
+        bullets.empty()
 
-    create_fleet(ai_settings, screen, ship, aliens)
-    ship.center_ship()
+        create_fleet(ai_settings, screen, ship, aliens)
+        ship.center_ship()
 
-    sleep(0.5)
+        sleep(0.5)
+    else:
+        stats.game_active = False
+
+
+def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
+    screen_rect = screen.get_rect()
+    for alien in aliens.sprites():
+        if alien.rect.bottom >= screen_rect.bottom:
+            ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+            break
 
 def update_aliens(ai_settings,stats,screen,ship, aliens, bullets):
     check_fleet_edges(ai_settings,aliens)
@@ -130,3 +141,4 @@ def update_aliens(ai_settings,stats,screen,ship, aliens, bullets):
     if pygame.sprite.spritecollideany(ship, aliens):
         ship_hit(ai_settings,stats,screen,ship, aliens, bullets)
         print("Ship hit!!")
+    check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets)
